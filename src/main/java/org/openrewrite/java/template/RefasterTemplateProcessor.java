c91617160625cd5a0605edb9e5c8feab4d35cf26
Knut Wannheden <knut@moderne.io>
2023-04-27 10:32:55 +0200

    Add missing trailing period in description.

--- src/main/java/org/openrewrite/java/template/RefasterTemplateProcessor.java
+++ src/main/java/org/openrewrite/java/template/RefasterTemplateProcessor.java
@@ -181,7 +181,7 @@ public class RefasterTemplateProcessor extends AbstractProcessor {
                             out.write("\n");
                             out.write("    @Override\n");
                             out.write("    public String getDescription() {\n");
-                            out.write("        return \"Recipe corresponding to the following Refaster template:\\n```\\n" + templateCode + "\\n```\";\n");
+                            out.write("        return \"Recipe corresponding to the following Refaster template:\\n```\\n" + templateCode + "\\n```.\";\n");
                             out.write("    }\n");
                             out.write("\n");
                             out.write("    @Override\n");

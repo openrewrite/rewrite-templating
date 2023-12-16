aab485e53d828ea8d31a007eba071f1c2bdb8df4
Tim te Beek <tim@moderne.io>
2023-12-16 20:00:00 +0100

    Remove JetBrains annotation

--- src/test/java/org/openrewrite/java/template/RefasterTemplateProcessorTest.java
+++ src/test/java/org/openrewrite/java/template/RefasterTemplateProcessorTest.java
@@ -19,7 +19,6 @@ import com.google.errorprone.refaster.annotation.AfterTemplate;
 import com.google.errorprone.refaster.annotation.BeforeTemplate;
 import com.google.testing.compile.Compilation;
 import com.google.testing.compile.JavaFileObjects;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openrewrite.java.template.processor.RefasterTemplateProcessor;
@@ -79,7 +78,6 @@ class RefasterTemplateProcessorTest {
           .hasSourceEquivalentTo(JavaFileObjects.forResource("refaster/" + recipeName + "Recipes.java"));
     }
 
-    @NotNull
     private static Compilation compile(String resourceName) {
         // As per https://github.com/google/compile-testing/blob/v0.21.0/src/main/java/com/google/testing/compile/package-info.java#L53-L55
         return javac()

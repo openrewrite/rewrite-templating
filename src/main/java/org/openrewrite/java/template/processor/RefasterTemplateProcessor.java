2c71a0e5c91d3ba0f6b0fc7dfc0820a57f8b5224
Tim te Beek <tim@moderne.io>
2023-12-17 21:57:42 +0100

    Non greedy match

--- src/main/java/org/openrewrite/java/template/processor/RefasterTemplateProcessor.java
+++ src/main/java/org/openrewrite/java/template/processor/RefasterTemplateProcessor.java
@@ -405,7 +405,7 @@ public class RefasterTemplateProcessor extends TypeAwareProcessor {
                 Tokens.Comment comment = cu.docComments.getComment(classDecl);
                 if (comment != null && comment.getText() != null && !comment.getText().isEmpty()) {
                     String commentText = comment.getText()
-                            .replaceAll("\\{@\\S+\\s+(.*)}", "`$1`")
+                            .replaceAll("\\{@\\S+\\s+(.*?)}", "`$1`")
                             .replace("\\", "\\\\")
                             .replace("\"", "\\\"")
                             .replace("\b", "\\b")

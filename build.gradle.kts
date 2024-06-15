0a6aff45b1c776a7f58992ab6c0cbf97b12f3376
Tim te Beek <tim@moderne.io>
2024-06-15 16:46:15 +0200

    Troubleshoot mismatched AssertJ method

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,6 +93,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
     testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+    testImplementation("org.assertj:assertj-core:latest.release")
 }
 
 tasks.withType<Test> {

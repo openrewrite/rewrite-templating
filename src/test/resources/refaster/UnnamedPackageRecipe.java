5c200fd92da849a134c82540a81c82a922cb1348
Tim te Beek <tim@moderne.io>
2024-09-07 16:27:45 +0200

    Inline return of new JavaVisitor (#109)

--- src/test/resources/refaster/UnnamedPackageRecipe.java
+++ src/test/resources/refaster/UnnamedPackageRecipe.java
@@ -58,7 +58,7 @@ public class UnnamedPackageRecipe extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        JavaVisitor<ExecutionContext> javaVisitor = new AbstractRefasterJavaVisitor() {
+        return new AbstractRefasterJavaVisitor() {
             final JavaTemplate before = JavaTemplate
                     .builder("\"This class is located in the default package\"")
                     .build();
@@ -81,6 +81,5 @@ public class UnnamedPackageRecipe extends Recipe {
             }
 
         };
-        return javaVisitor;
     }
 }

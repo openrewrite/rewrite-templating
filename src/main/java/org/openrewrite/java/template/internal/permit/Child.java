6031b4f956fd536f612e914b167281fc43f508bb
Knut Wannheden <knut@moderne.io>
2025-06-17 06:42:15 +0200

    Update Child.java

--- src/main/java/org/openrewrite/java/template/internal/permit/Child.java
+++ src/main/java/org/openrewrite/java/template/internal/permit/Child.java
@@ -17,5 +17,7 @@ package org.openrewrite.java.template.internal.permit;
 
 @SuppressWarnings("all")
 public abstract class Child extends Parent {
-
+	private transient volatile boolean foo;
+	private transient volatile Object[] bar;
+	private transient volatile Object baz;
 }

6262261f6fdd34175319da8ed827b7f23895d94b
Laurens Westerlaken <laurens.westerlaken@moderne.io>
2025-07-02 08:34:22 -0400

    Update regarding maven central (#150)

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,10 @@ configurations.all {
 
 nexusPublishing {
     repositories {
-        sonatype()
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
     }
 }
 

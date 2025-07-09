d05b05ad1e8163fd5975a2845a63c30bcd2d14bc
Tim te Beek <tim@moderne.io>
2025-07-09 16:30:13 +0200

    Update build.gradle.kts to use 5.13.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,8 +100,8 @@ dependencies {
     testImplementation("jakarta.annotation:jakarta.annotation-api:2.+")
     testImplementation("javax.annotation:javax.annotation-api:1.+")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.+")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.+")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.3")
 }
 

33a07c3dfbd77b18d6e9316042870acd5d197537
Tim te Beek <tim@moderne.io>
2025-08-16 16:40:16 +0200

    Use Java 21 for tests (#162)
    * Use Java 21 in the tests
    
    * Minimize build flags
    
    * Add missing newline
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Apply code suggestions now that we use text blocks
    
    * Use lazy initialization
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/template/internal/AbstractRefasterJavaVisitorTest.java
+++ src/test/java/org/openrewrite/java/template/internal/AbstractRefasterJavaVisitorTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.template.internal;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.tree.J;
@@ -27,55 +28,58 @@ import static org.openrewrite.java.template.internal.AbstractRefasterJavaVisitor
 import static org.openrewrite.test.RewriteTest.toRecipe;
 
 class AbstractRefasterJavaVisitorTest implements RewriteTest {
+    @DocumentExample
     @Test
     void useStaticImports() {
         rewriteRun(
-          spec -> spec.recipe(
-            toRecipe(
-              () -> new AbstractRefasterJavaVisitor() {
-                  private final JavaTemplate template = JavaTemplate
-                    .builder("#{path:any(java.nio.file.Path)}.toFile().exists()")
-                    .build();
+          spec -> spec.recipe(toRecipe(FilesExistsVisitor::new)),
+          java(
+            """
+              import java.nio.file.Path;
 
-                  @Override
-                  public J visitMethodInvocation(J.MethodInvocation elem, ExecutionContext ctx) {
-                      JavaTemplate.Matcher matcher;
-                      if ((matcher = template.matcher(getCursor())).find()) {
-                          return embed(
-                            JavaTemplate.apply(
-                              "java.nio.file.Files.exists(#{path:any(java.nio.file.Path)})",
-                              getCursor(),
-                              elem.getCoordinates().replace(),
-                              matcher.parameter(0)
-                            ),
-                            getCursor(),
-                            ctx,
-                            SHORTEN_NAMES, STATIC_IMPORT_ALWAYS
-                          );
-                      }
-                      return super.visitMethodInvocation(elem, ctx);
+              class A {
+                  boolean pathExists(Path path) {
+                      return path.toFile().exists();
                   }
               }
-            )
-          ),
-          java(
-            "import java.nio.file.Path;\n" +
-              "\n" +
-              "class A {\n" +
-              "    boolean pathExists(Path path) {\n" +
-              "        return path.toFile().exists();\n" +
-              "    }\n" +
-              "}",
-            "import java.nio.file.Path;\n" +
-              "\n" +
-              "import static java.nio.file.Files.exists;\n" +
-              "\n" +
-              "class A {\n" +
-              "    boolean pathExists(Path path) {\n" +
-              "        return exists(path);\n" +
-              "    }\n" +
-              "}"
+              """,
+            """
+              import java.nio.file.Path;
+
+              import static java.nio.file.Files.exists;
+
+              class A {
+                  boolean pathExists(Path path) {
+                      return exists(path);
+                  }
+              }
+              """
           )
         );
+    }
+
+    private static class FilesExistsVisitor extends AbstractRefasterJavaVisitor {
+        private final JavaTemplate template = JavaTemplate
+          .builder("#{path:any(java.nio.file.Path)}.toFile().exists()")
+          .build();
+
+        @Override
+        public J visitMethodInvocation(J.MethodInvocation elem, ExecutionContext ctx) {
+            JavaTemplate.Matcher matcher;
+            if ((matcher = template.matcher(getCursor())).find()) {
+                return embed(
+                  JavaTemplate.apply(
+                    "java.nio.file.Files.exists(#{path:any(java.nio.file.Path)})",
+                    getCursor(),
+                    elem.getCoordinates().replace(),
+                    matcher.parameter(0)
+                  ),
+                  getCursor(),
+                  ctx,
+                  SHORTEN_NAMES, STATIC_IMPORT_ALWAYS
+                );
+            }
+            return super.visitMethodInvocation(elem, ctx);
+        }
     }
 }
